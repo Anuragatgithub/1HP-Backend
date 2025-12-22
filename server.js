c38821e8b5fd3c2f2e066a3add27136842e76260
Anuragatgithub <anuragyadav152013@gmail.com>
2025-12-22 17:46:33 +0530

    Fix port for Render

--- server.js
+++ server.js
@@ -27,4 +27,9 @@ res.status(500).json({success:false});
 }
 });
 
-app.listen(5000,()=>console.log("Backend running on 5000"));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Backend running on ${PORT}`);
+});
+
